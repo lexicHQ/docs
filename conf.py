8725793c1d339fe07a537cd81fdafe098559b7ed
Mehfuz Hossain <mehfuz@gmail.com>
2017-11-29 01:34:00 -0800

    fix

--- conf.py
+++ conf.py
@@ -186,6 +186,7 @@ html_theme_options = {
 # }
 
 html_show_sourcelink = False
+html_favicon = 'favicon.ico' 
 # html_static_path = ['_static']
 # html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 def setup(app):
